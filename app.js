1a8ab0110e91300b3488bf77ef8665ba6e979d87
nikuldchauhan <dhaval301092@gmail.com>
2015-04-07 03:00:00 +0100

    Step 1 &2 working

--- app.js
+++ app.js
@@ -1,45 +1,85 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var redis = require('redis');
+var url=require('url');
 var client = redis.createClient();
-
+var bases=require('bases');
 var app = express();
 
 app.use(express.static('public'));
-var resultArray = '{"outcome":"result","wins":0,"losses":0,"ties":0}';
+//var resultArray;
+//var obj='{}';
+
+var num=479890;
 //var gameArray = ["rock", "paper", "scissors", "lizard", "spock"];
 //var resultJSON = JSON.parse(resultArray);
-
+//obj["479890"]="http://google.com";
+//resultArray.push(obj);
+//console.log(resultArray);
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
 client.on('error', function(error)
 {
-console.log("Error " + error);
+    console.log("Error " + error);
+});
 
+client.exists('counter', function(err, reply) {
+	 if (reply != 1) {
+		{
+			client.set('counter', 479890);
+		}
+	}
 });
 
 
-app.post('/linksubmit', function(req,res) {
-    //console.log('here');
-    /*
-    client.set('vege','Onion'­,redis.print);
-    client.get('vege',function(error­,value)
-    {
-    if(error)
-    {
-    throw error;
-    }
-    console.log('The vegetable is = '+ value);
+//console.log(bases.toBase36(479891));
 
-    });
-    */
-    console.log(req.body.link);
-    res.write(req.body.link);
-    res.end();
-    //res.json(resultJSON);
-    //console.log(resultJSON);
-})
+app.post('/request', function(req,res) {
+	var link=req.body.link;
+	
+	var counter;
+	parseurl=url.parse(link).pathname;
+	console.log("parse url "+parseurl);
+	console.log(parseurl.slice(1));
+	
+	client.exists(link, function(err, reply) {
+
+
+    if (reply === 1) {
+    	client.get(link, function(err, reply) {
+    		res.send("Already Created <br/> http://localhost:3000/"+reply);
+    		//res.send(reply);
+    	});
+    }
+    else {
+        //console.log('doesn\'t exist');
+        client.get('counter', function(err, reply) {
+		counter=reply;
+	var shorturl=bases.toBase36(counter);
+	console.log(shorturl);
+	client.set(link,shorturl);
+	client.set(shorturl,link);
+	client.set('counter', Number(counter)+1);
+
+	res.send("http://localhost:3000/"+ shorturl);
+	});
+
+    }
+	});
+	
+	/*
+	client.get(link, function(err, reply) {
+
+			//console.log("short link  " + reply);
+			//res.send("http://localhost:3000/"+shorturl);
+	});
+
+	client.get(shorturl, function(err, reply) {
+			console.log("Ori link "  + reply);
+	});
+	*/
+});
 
 app.listen(3000);
--- public/js/client.js
+++ public/js/client.js
@@ -21,26 +21,14 @@ $("#linkForm").submit(function(event) {
   }
   //console.log(link);
   $.ajax({
-        url: '/linksubmit',
+        url: '/request',
         data:{'link':link},
         type: 'post',
         //dataType: 'json',
         //contentType: "application/json; charset=utf-8",
         success: function(data) {
             console.log(data);
-            
-            /*
-            if (data.outcome === "win") {
-                $("#info").html('<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Won!</strong>You won for <strong>' + id + '</strong>.</div>');
-            } else if (data.outcome === "losses") {
-                $("#info").html('<div class="alert alert-danger alert-dismissible"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Loss!</strong>You loss for <strong>' + id + '</strong>.</div>');
-            } else if (data.outcome === "ties") {
-                $("#info").html('<div class="alert alert-warning alert-dismissible"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Tie!</strong>You tie for <strong>' + id + '</strong>.</div>');
-            } else {
-                $("#info").html();
-            }
-            $("#result").html(renderjson.set_show_by_default(true)(data));
-            */
+            $("#info").html(data);
         }
       });
 });
