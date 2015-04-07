a275da0f8d3c2a1062364f2d6fb0b3001a1b5284
nikuldchauhan <dhaval301092@gmail.com>
2015-04-07 07:00:00 +0100

    JSHint And Beutified
    JSHint only gives err and require removing hint but removal of it generate error

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var client = redis.createClient();
 var bases = require('bases');
 var url = require('url');
 var app = express();
-app.use(express.static(__dirname + '/public', { maxAge: 20 }));
-var num = 479890;
+app.use(express.static(__dirname + '/public', {
+    maxAge: 20
+}));
+//var num = 479890;
 app.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -16,23 +18,21 @@ client.on('error', function(error) {
 });
 
 client.exists('counter', function(err, reply) {
+    if(err)console.log(err);
     if (reply != 1) {
         client.set('counter', 479890);
     }
 });
-
-
-//console.log(bases.toBase36(479891));
-
 app.post('/request', function(req, res) {
     var link = req.body.link;
     var parseurl = url.parse(link).pathname.slice(1);
     var hostname = url.parse(link).hostname;
     if (hostname == "localhost") {
         client.exists(parseurl, function(err, reply) {
+            if(err)console.log(err);
             if (reply === 1) {
                 client.get(parseurl, function(err, reply) {
-                    //res.send("Already Created <br/> http://localhost:3000/"+reply);
+                  if(err)console.log(err);
                     res.send(reply);
                 });
             } else {
@@ -41,26 +41,22 @@ app.post('/request', function(req, res) {
         });
 
     } else {
-        //console.log("parse_url"+parseurl);     
-        //console.log("hostname"+url.parse(link).hostname);
         var counter;
-
         client.exists(link, function(err, reply) {
+            if(err)console.log(err);
             if (reply === 1) {
                 client.get(link, function(err, reply) {
-                    res.send("Already Created <br/> http://localhost:3000/" + reply);
-                    //res.send(reply);
+                    if(err)console.log(err);
+                    res.send("Already Created <br/> http://localhost:3000/" + reply+"<br/>");
                 });
             } else {
-                //console.log('doesn\'t exist');
                 client.get('counter', function(err, reply) {
+                    if(err) console.log(err);
                     counter = reply;
                     var shorturl = bases.toBase36(counter);
-                    //console.log(shorturl);
                     client.set(link, shorturl);
                     client.set(shorturl, link);
                     client.set('counter', Number(counter) + 1);
-
                     res.send("http://localhost:3000/" + shorturl);
                 });
 
@@ -69,14 +65,12 @@ app.post('/request', function(req, res) {
     }
 });
 app.route("/:url").all(function(req, res) {
-    
     var url = req.params.url;
-    //console.log(url);
     client.exists(url, function(err, reply) {
+        if(err)console.log(err);
         if (reply === 1) {
-            //console.log("here");
             client.get(url, function(err, reply) {
-                
+                                if(err)console.log(err);
                 client.zincrby('views', 1, url);
                 res.status(301);
                 res.setHeader("Cache-Control", "no-cache");
@@ -84,38 +78,15 @@ app.route("/:url").all(function(req, res) {
                 res.send();
             });
         } else {
-            console.log("hsdfsd");
             res.send("URL not found");
         }
     });
 });
-
-client.keys('*', function(err, keys) {
-    if (err) return console.log(err);
-
-    for (var i = 0, len = keys.length; i < len; i++) {
-        var hostname = url.parse(keys[i]).hostname;
-        if (hostname === null && keys[i] != 'counter') {
-           // console.log(keys[i]);
-        }
-    }
-});
-
 app.get('/result/urls', function(req, res) {
-    //var members=[];
-client.zrevrangebyscore('views', "+inf", 0, "limit", 0, 10,function(err, members) {
-        console.log(members);
+    console.log("inside"+req.url)
+    client.zrevrangebyscore('views', "+inf", 0, "limit", 0, 10, function(err, members) {
+        if(err)console.log(err);
         res.send(members);
+    });
 });
-});
-/*
-client.hmset("hosts", "mjr", "91");
-client.hmset("hosts","another", "23", "home", "1234");
-
-//client.zincrby("hosts", 1, "mjr");
-//client.zincrby("hosts", 1, "mjr");
-client.hgetall("hosts", function (err, obj) {
-    console.dir(obj);
-});
-*/
 app.listen(3000);
--- public/js/client.js
+++ public/js/client.js
@@ -1,42 +1,34 @@
-
 $(document).ready(function() {
-  $.ajax({
-      url: '/result/urls',
-      type: 'get',
-      dataType: 'json',
-      contentType: "application/json; charset=utf-8",
-      success: function(data) {
-         $("#result").html("<h1>Top 10 URLs</h1>");
-        var arrayLength = data.length;
-        for(var i=0;i<arrayLength;i++)
-        {
-            $("#result").append("<a href='http://localhost:3000/"+data[i]+"'>http://localhost:3000/"+data[i]+"</a><br/>");
+    $.ajax({
+        url: '/result/urls',
+        type: 'get',
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            $("#result").html("<h1>Top 10 URLs</h1>");
+            var arrayLength = data.length;
+            for (var i = 0; i < arrayLength; i++) {
+                $("#result").append("<a href='http://localhost:3000/" + data[i] + "'>http://localhost:3000/" + data[i] + "</a><br/>");
+            }
         }
-          //$("#result").html(renderjson.set_show_by_default(true)(data));
-        
-      }
-  });
+    });
 });
 $("#linkForm").submit(function(event) {
-  event.preventDefault();
-  var link = $("#link").val();
-  if(/^([a-z]([a-z]|\d|\+|-|\.)*):(\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?((\[(|(v[\da-f]{1,}\.(([a-z]|\d|-|\.|_|~)|[!\$&'\(\)\*\+,;=]|:)+))\])|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=])*)(:\d*)?)(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*|(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)){0})(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(link)) {
-  //alert("valid url");
-  } else {
-  alert("invalid url");
-  }
-  //console.log(link);
-  $.ajax({
+    event.preventDefault();
+    var link = $("#link").val();
+    if (/^([a-z]([a-z]|\d|\+|-|\.)*):(\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?((\[(|(v[\da-f]{1,}\.(([a-z]|\d|-|\.|_|~)|[!\$&'\(\)\*\+,;=]|:)+))\])|((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=])*)(:\d*)?)(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*|(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)|((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)){0})(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(link)) {
+    } else {
+        alert("invalid url");
+    }
+    $.ajax({
         url: '/request',
-        data:{'link':link},
+        data: {
+            'link': link
+        },
         type: 'post',
-        //dataType: 'json',
-        //contentType: "application/json; charset=utf-8",
         success: function(data) {
             console.log(data);
             $("#info").html(data);
         }
-      });
+    });
 });
-
-//$("#")
